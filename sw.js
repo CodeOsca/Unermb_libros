97cd4e810efec14ce8f7218ab1bbeff73ece02b0
CodeOsca <oscarcolinafs@gmail.com>
2025-10-31 17:31:27 -0400

    configuraciones regulares agregadas

--- sw.js
+++ sw.js
@@ -1,25 +1,62 @@
 // sw.js
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('mi-cache').then(cache => {
-        return cache.addAll([
-            './index.html',
-            './css/accordion.css',
-            './css/button_material.css',
-            './css/index.css',
-            './css/input_material.css',
-            './css/page.css',
-            './js/flashy.js',
-            './js/search.js'
-        ]);
-        })
-    );
-});
+const CACHE_NAME = 'v1_UNERMB_LIBROS_APP';
+const urlsToCache = [
+    './index.html',
+    './css/accordion.css',
+    './css/button_material.css',
+    './css/index.css',
+    './css/input_material.css',
+    './css/page.css',
+    './js/flashy.js',
+    './js/search.js'
+]
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache)
+            .then(() => self.skipWaiting())
         })
-    );
-});
+        .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+
+    e.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+            cacheNames.map(cacheName => {
+                //Eliminamos lo que ya no se necesita en cache
+                if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName)
+                }
+            })
+            )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+    )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if (res) {
+            //recuperar del cache
+            return res
+            }
+            //recuperar de la petición a la url
+            return fetch(e.request)
+        })
+    )
+})
