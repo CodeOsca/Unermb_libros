490eb5dadc107c43b07ff5c9f1378a46ed159f47
CodeOsca <oscarcolinafs@gmail.com>
2025-10-31 17:20:45 -0400

    correcion en sw

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('mi-cache').then(cache => {
         return cache.addAll([
-            '/',
             './index.html',
             './css/accordion.css',
             './css/button_material.css',
