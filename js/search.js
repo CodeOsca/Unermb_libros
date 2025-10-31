de5cbadd0e6b0378072d4f7664f0e56c40e613f8
CodeOsca <oscarcolinafs@gmail.com>
2025-10-31 13:31:26 -0400

    problema de manifest solucionado

--- js/search.js
+++ js/search.js
@@ -116,10 +116,3 @@ document.getElementById("searchForm").addEventListener("submit", function (e) {
     
 });
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('✅ Service Worker registrado:', reg.scope))
-        .catch(err => console.error('❌ Error al registrar el Service Worker:', err));
-    });
-}
